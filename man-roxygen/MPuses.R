5ca08270a48b6c3a62a284af2fe33e8c23ca8ed5
Adrian <ar.hordyk@gmail.com>
2021-01-07 14:11:17 -0800

    Update MPuses.R

--- man-roxygen/MPuses.R
+++ man-roxygen/MPuses.R
@@ -2,6 +2,6 @@
 #' <%= paste0("\\code{", MSEtool::Required(mp, TRUE)[,1], "}: ", MSEtool::Required(mp, TRUE)[,2]) %>
 #'
 #' @section Rendered Equations:
-#' <%= if(MPurl(mp, nameonly=TRUE) == mp) paste0("See \\href{", MPurl(mp), "}{Online Documentation} for correctly rendered equations") %>
+#' <%= if(MSEtool::MPurl(mp, nameonly=TRUE) == mp) paste0("See \\href{", MSEtool::MPurl(mp), "}{Online Documentation} for correctly rendered equations") %>
 
 
