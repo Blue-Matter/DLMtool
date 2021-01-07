76e9dd9170d6140e635a5e6f4e51b5bd3e4384da
Adrian <ar.hordyk@gmail.com>
2021-01-07 14:11:14 -0800

    Update MPtemplate.R

--- man-roxygen/MPtemplate.R
+++ man-roxygen/MPtemplate.R
@@ -7,10 +7,10 @@
 #' See \linkS4class{Data} for information on the \code{Data} object \cr
 #'
 #' @return
-#'  <%= if(MSEtool::MPtype(mp)[2] == 'Output') paste0("An object of class \\code{\\link[=Rec-class]{Rec}} with the \\code{TAC} slot populated with a numeric vector of length \\code{reps}") %>
-#'  <%= if(MSEtool::MPtype(mp)[2] == 'Reference') paste0("An object of class \\code{\\link[=Rec-class]{Rec}} with the \\code{TAC} slot populated with a numeric vector of length \\code{reps}") %>
-#'  <%= if(MSEtool::MPtype(mp)[2] == 'Input') paste0("An object of class \\code{\\link[=Rec-class]{Rec}} with the ", paste(MSEtool::MPtype(mp)[3], collapse=','), " slot(s) populated") %>
-#'  <%= if(MSEtool::MPtype(mp)[2] == 'Mixed') paste0("An object of class \\code{\\link[=Rec-class]{Rec}} with the ", paste(MSEtool::MPtype(mp)[3], collapse=','), " slot(s) populated") %>
+#'  <%= if(MSEtool::MPtype(mp)[2] == 'Output') paste0("An object of class \\code{\\link[MSEtool]{Rec-class}} with the \\code{TAC} slot populated with a numeric vector of length \\code{reps}") %>
+#'  <%= if(MSEtool::MPtype(mp)[2] == 'Reference') paste0("An object of class \\code{\\link[MSEtool]{Rec-class}} with the \\code{TAC} slot populated with a numeric vector of length \\code{reps}") %>
+#'  <%= if(MSEtool::MPtype(mp)[2] == 'Input') paste0("An object of class \\code{\\link[MSEtool]{Rec-class}} with the ", paste(MSEtool::MPtype(mp)[3], collapse=','), " slot(s) populated") %>
+#'  <%= if(MSEtool::MPtype(mp)[2] == 'Mixed') paste0("An object of class \\code{\\link[MSEtool]{Rec-class}} with the ", paste(MSEtool::MPtype(mp)[3], collapse=','), " slot(s) populated") %>
 #'
 
 
