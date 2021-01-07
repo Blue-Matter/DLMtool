6b78eb10ec27676960cbb5e5eeef74740b9dbc7b
Adrian <ar.hordyk@gmail.com>
2021-01-07 15:05:13 -0800

    Update aaImportFrom.R

--- R/aaImportFrom.R
+++ R/aaImportFrom.R
@@ -1,7 +1,10 @@
+
 #' @importFrom dplyr %>%
+#' @importFrom ggplot2 aes expand_limits geom_boxplot ggplot labs theme_classic
 #' @importFrom graphics abline axis barplot boxplot legend lines matplot mtext
 #' par points text title
 #' @importFrom methods new
+#' @importFrom Rcpp evalCpp
 #' @importFrom stats approx coef dbeta dnorm lm loess median optim optimize
 #' predict quantile rbeta rlnorm rnorm runif sd
 NULL
