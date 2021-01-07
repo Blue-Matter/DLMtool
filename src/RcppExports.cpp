2e9b43a292b546702e266b68004ce8acde132295
Adrian <ar.hordyk@gmail.com>
2021-01-07 15:05:43 -0800

    Update RcppExports.cpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -56,10 +56,29 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// bhnoneq_LL
+double bhnoneq_LL(NumericVector stpar, NumericVector year, NumericVector Lbar, NumericVector ss, double Linf, double K, double Lc, int nbreaks);
+RcppExport SEXP _DLMtool_bhnoneq_LL(SEXP stparSEXP, SEXP yearSEXP, SEXP LbarSEXP, SEXP ssSEXP, SEXP LinfSEXP, SEXP KSEXP, SEXP LcSEXP, SEXP nbreaksSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< NumericVector >::type stpar(stparSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type year(yearSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type Lbar(LbarSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type ss(ssSEXP);
+    Rcpp::traits::input_parameter< double >::type Linf(LinfSEXP);
+    Rcpp::traits::input_parameter< double >::type K(KSEXP);
+    Rcpp::traits::input_parameter< double >::type Lc(LcSEXP);
+    Rcpp::traits::input_parameter< int >::type nbreaks(nbreaksSEXP);
+    rcpp_result_gen = Rcpp::wrap(bhnoneq_LL(stpar, year, Lbar, ss, Linf, K, Lc, nbreaks));
+    return rcpp_result_gen;
+END_RCPP
+}
 
 static const R_CallMethodDef CallEntries[] = {
     {"_DLMtool_LBSPRgen", (DL_FUNC) &_DLMtool_LBSPRgen, 16},
     {"_DLMtool_LBSPRopt", (DL_FUNC) &_DLMtool_LBSPRopt, 15},
+    {"_DLMtool_bhnoneq_LL", (DL_FUNC) &_DLMtool_bhnoneq_LL, 8},
     {NULL, NULL, 0}
 };
 
