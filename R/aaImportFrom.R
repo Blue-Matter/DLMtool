86278f24fadf990a7fb3caf701319076859125c5
Adrian <ar.hordyk@gmail.com>
2021-01-07 14:11:30 -0800

    Create aaImportFrom.R

---
+++ R/aaImportFrom.R
@@ -0,0 +1,7 @@
+#' @importFrom dplyr %>%
+#' @importFrom graphics abline axis barplot boxplot legend lines matplot mtext
+#' par points text title
+#' @importFrom methods new
+#' @importFrom stats approx coef dbeta dnorm lm loess median optim optimize
+#' predict quantile rbeta rlnorm rnorm runif sd
+NULL
