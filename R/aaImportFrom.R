9527707c123e3221b779347f2e14c5df0720d854
Adrian <ar.hordyk@gmail.com>
2021-01-11 08:37:15 -0800

    Update aaImportFrom.R

--- R/aaImportFrom.R
+++ R/aaImportFrom.R
@@ -2,7 +2,7 @@
 #' @importFrom dplyr %>%
 #' @importFrom ggplot2 aes expand_limits geom_boxplot ggplot labs theme_classic
 #' @importFrom graphics abline axis barplot boxplot legend lines matplot mtext
-#' par points text title
+#' par plot points text title
 #' @importFrom methods new
 #' @importFrom MSEtool TACfilter
 #' @importFrom Rcpp evalCpp
