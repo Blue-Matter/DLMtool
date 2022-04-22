c99d5c41c272cf96426a8c359ac06f163155f31a
Adrian <ar.hordyk@gmail.com>
2022-04-22 13:40:17 -0700

    Update LBSPR_test.hpp

--- src/TMB/LBSPR_test.hpp
+++ src/TMB/LBSPR_test.hpp
@@ -99,7 +99,7 @@ Type LBSPR_test(objective_function<Type>* obj) {
     }
   }
 
-  Type nres = res.len();
+  Type nres = res.size();
   Type mures = res.sum()/nres;
   vector<Type> temp(nlen);
   temp.setZero();
